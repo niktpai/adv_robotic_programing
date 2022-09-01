1983584995ec2dd3e84be5e50cecc030866fa0e0
Nikhil <98666935+niktpai@users.noreply.github.com>
2022-09-01 19:06:46 -0400

    Wrote functions to create bel and bel_bar arrays

---
+++ hw2.py
@@ -0,0 +1,50 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# VARIABLES
+prob_moves = 0.7
+prob_stays = 0.2
+prob_moves_2 = 0.1
+
+prob_door_wall = 0.3
+prob_door_door = 0.8
+
+def bel_bar(belief):
+    '''
+    Function that takes in an n-dim belief array and returns a belief bar array of equal size
+    '''
+    bel_bar = np.zeros(shape=belief.shape, dtype = "float64")
+    for n in range(belief.shape[0]):
+        if n == 0:
+            bel_bar[n] = prob_stays *belief[n]
+        elif n == 1:
+            bel_bar[n] = prob_moves * belief[n-1] + prob_stays *belief[n]
+        else:
+            bel_bar[n] = prob_moves_2 * belief[n-2] + prob_moves * belief[n-1] + prob_stays *belief[n]
+    return bel_bar
+
+def bel(belief_bar):
+    '''
+    Function that takes in an n-dim belief bar array and returns a belief array of equal size
+    '''
+    bel = np.zeros(shape=belief_bar.shape, dtype = "float64")
+    for n in range(belief_bar.shape[0]):
+        if n%2 == 0:
+            bel[n] = prob_door_wall *belief_bar[n]
+            
+        elif n%2 ==1:
+            bel[n] = prob_door_door *belief_bar[n]
+    n = 1/sum(bel)        
+    bel = bel * n
+    return bel
+
+belief_bar_0 = 0.25 * np.ones(4)
+belief_0 = belief_bar_0
+
+belief_bar_1 = bel_bar(belief_bar_0)
+belief_1 = bel(belief_bar_1)
+
+belief_bar_2 = bel_bar(belief_bar_1)
+belief_2 = bel(belief_bar_2)
+
+print(belief_2)
