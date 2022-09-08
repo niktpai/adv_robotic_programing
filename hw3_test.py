f8571a3017362de0822d9e1ddcf6334e985ae374
dgrace6 <86323276+dgrace6@users.noreply.github.com>
2022-09-08 12:08:50 -0400

    Update hw3_test.py

--- hw3_test.py
+++ hw3_test.py
@@ -7,8 +7,8 @@ while True:
 
     hsvBlue = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    lower_blue = np.array([10, 10, 10])
-    upper_blue = np.array([344, 150, 150])
+    lower_blue = np.array([0, 0, 0])
+    upper_blue = np.array([60, 100, 100])
 
     mask = cv2.inRange(hsvBlue, lower_blue, upper_blue)
 
@@ -22,5 +22,4 @@ while True:
     if cv2.waitKey(20) == ord('q'):
         break
 
-vidcap.release()
 cv2.destroyAllWindows()
